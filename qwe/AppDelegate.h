e5b35625699dcb49aa916a9d2c172e9bdad9cb19
FreshWater <763335307@qq.com>
2017-07-05 20:22:34 +0800

    Initial Commit

---
+++ qwe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qwe
+//
+//  Created by FreshWater on 17/7/5.
+//  Copyright © 2017年 FreshWater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qwe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qwe
+//
+//  Created by FreshWater on 17/7/5.
+//  Copyright © 2017年 FreshWater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
